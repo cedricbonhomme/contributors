4f08ea9a38795177486a283bf96dd1d6397a0ec9
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-07-17 02:20:30 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,44 @@
+
+
+## Contributors
+
+### MISP
+
+<!-- contributors-MISP starts -->
+<a href="https://github.com/Jakub Onderka"><img src="https://avatars3.githubusercontent.com/u/163343?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/iglocska"><img src="https://avatars1.githubusercontent.com/u/3668672?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/chrisr3d"><img src="https://avatars2.githubusercontent.com/u/16307976?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/mokaddem"><img src="https://avatars3.githubusercontent.com/u/6977223?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/Steve Clement"><img src="https://avatars3.githubusercontent.com/u/362025?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/Richard van den Berg"><img src="https://avatars1.githubusercontent.com/u/1461970?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/Alexandre Dulaunoy"><img src="https://avatars2.githubusercontent.com/u/3309?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<a href="https://github.com/Alex Resnick"><img src="https://avatars2.githubusercontent.com/u/13125104?s=100&v=4" alt="drawing" style="width:100px;"/></a>
+<!-- contributors-MISP ends -->
+
+
+### PyMISP
+
+<!-- contributors-PyMISP starts -->
+![Raphaël Vinot](https://avatars1.githubusercontent.com/u/248875?s=100&v=4)
+![VVX7](https://avatars2.githubusercontent.com/u/46228229?s=100&v=4)
+![Christophe Vandeplas](https://avatars0.githubusercontent.com/u/1073662?s=100&v=4)
+![Sebastian Wagner](https://avatars1.githubusercontent.com/u/25031221?s=100&v=4)
+<!-- contributors-PyMISP ends -->
+
+
+### MONARC
+
+<!-- contributors-MONARC starts -->
+![Cédric Bonhomme](https://avatars1.githubusercontent.com/u/465400?s=100&v=4)
+![Ruslan Baidan](https://avatars2.githubusercontent.com/u/3246171?s=100&v=4)
+![Jerome Lombardi](https://avatars1.githubusercontent.com/u/18661517?s=100&v=4)
+<!-- contributors-MONARC ends -->
+
+
+### Stegano
+
+<!-- contributors-stegano starts -->
+![Cédric Bonhomme](https://avatars1.githubusercontent.com/u/465400?s=100&v=4)
+![AdrienCos](https://avatars1.githubusercontent.com/u/25573947?s=100&v=4)
+![Peter Justin](https://avatars2.githubusercontent.com/u/1510708?s=100&v=4)
+<!-- contributors-stegano ends -->
---
+++ authors.py
@@ -0,0 +1,149 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import pathlib
+from collections import Counter
+from python_graphql_client import GraphqlClient
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+
+TOKEN = os.environ.get("CONTRIBUTORS_TOKEN", "")
+
+
+def replace_chunk(content, marker, chunk):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def make_query(after_cursor=None, owner="", name=""):
+    query = """
+query {
+  repository(owner: "OWNER", name: "NAME") {
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(after: null) {
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+            totalCount
+            edges {
+              node {
+                ... on Commit {
+                  committer {
+                    name
+                    email
+                    user {
+                      name
+                    }
+                    date
+                    avatarUrl(size: 100)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+    query = query.replace("OWNER", owner)
+    query = query.replace("NAME", name)
+    return query
+
+
+def fetch_contributors(oauth_token, owner, name):
+    has_next_page = True
+    after_cursor = None
+
+    contributors_elem = {}
+    contributors_cnt = Counter()
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor, owner, name),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+
+        for node in data["data"]["repository"]["defaultBranchRef"]["target"]["history"][
+            "edges"
+        ]:
+            if None is not node["node"]["committer"]["user"]:
+                if not contributors_elem.get(node["node"]["committer"]["name"], False):
+                    contributors_elem[node["node"]["committer"]["email"]] = node["node"]["committer"]
+                    contributors_cnt[node["node"]["committer"]["email"]] += 1
+
+        has_next_page = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["pageInfo"][
+            "hasNextPage"
+        ]
+        after_cursor = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["pageInfo"][
+            "endCursor"
+        ]
+
+    return contributors_elem, contributors_cnt
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+
+    contributors_elem, contributors_cnt = fetch_contributors(TOKEN, "MISP", "MISP")
+    commons = contributors_cnt.most_common(100)
+    md = "\n".join(
+        [
+            '<a href="https://github.com/{0}"><img src="{1}" alt="drawing" style="width:100px;"/></a>'.format(contributors_elem[contributor]["name"], contributors_elem[contributor]["avatarUrl"])
+            for contributor, count in commons
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(readme_contents, "contributors-MISP", md)
+    
+    
+    contributors_elem, contributors_cnt = fetch_contributors(TOKEN, "MISP", "PyMISP")
+    commons = contributors_cnt.most_common(100)
+    md = "\n".join(
+        [
+            '<a href="https://github.com/{0}"><img src="{1}" alt="drawing" style="width:100px;"/></a>'.format(contributors_elem[contributor]["name"], contributors_elem[contributor]["avatarUrl"])
+            for contributor, count in commons
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(rewritten, "contributors-PyMISP", md)
+    
+    
+    contributors_elem, contributors_cnt = fetch_contributors(TOKEN, "monarc-project", "MonarcAppFO")
+    commons = contributors_cnt.most_common(100)
+    md = "\n".join(
+        [
+            '<a href="https://github.com/{0}"><img src="{1}" alt="drawing" style="width:100px;"/></a>'.format(contributors_elem[contributor]["name"], contributors_elem[contributor]["avatarUrl"])
+            for contributor, count in commons
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(rewritten, "contributors-MONARC", md)
+
+
+    contributors_elem, contributors_cnt = fetch_contributors(TOKEN, "cedricbonhomme", "stegano")
+    commons = contributors_cnt.most_common(100)
+    md = "\n".join(
+        [
+            '<a href="https://github.com/{0}"><img src="{1}" alt="drawing" style="width:100px;"/></a>'.format(contributors_elem[contributor]["name"], contributors_elem[contributor]["avatarUrl"])
+            for contributor, count in commons
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(rewritten, "contributors-stegano", md)
+
+
+    readme.open("w").write(rewritten)
